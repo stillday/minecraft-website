4ad140c056946b93c11c34a6b33be5e39b324b39
JD <jan-dennisd@outlook.at>
2018-09-09 19:44:38 +0200

    starting with coding for shadowbox

---
+++ js/shadowbox.js
@@ -0,0 +1,7 @@
+function shadowbox(ID, btn) {
+    var modal = document.getElementById('ID');
+    var button = document.getElementById('btn');
+    var span = document.getElementsByClassName('close')[0];
+
+    
+}
